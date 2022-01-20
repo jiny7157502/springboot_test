d1c09e9634077cae6cd3564ec35c33f83f4d012a
LAPTOP-8GDS7GCB\jiny7 <jiny7157502@naver.com>
2022-01-20 21:17:15 +0900

    test code commit

---
+++ src/main/java/example/org/springboot_test/Application.java
@@ -0,0 +1,11 @@
+package example.org.springboot_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
