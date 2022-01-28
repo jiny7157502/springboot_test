443bba355a4c8b7cb19619129f260fdd1adcdfbd
LAPTOP-8GDS7GCB\jiny7 <jiny7157502@naver.com>
2022-01-28 15:29:40 +0900

    board 등록/수정/삭제 commit

--- src/main/java/example/org/springboot_test/Application.java
+++ src/main/java/example/org/springboot_test/Application.java
@@ -2,7 +2,9 @@ package example.org.springboot_test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
---
+++ src/main/java/example/org/springboot_test/web/PostsApiController.java
@@ -0,0 +1,30 @@
+package example.org.springboot_test.web;
+
+import example.org.springboot_test.service.posts.PostsService;
+import example.org.springboot_test.web.dto.PostsResponseDto;
+import example.org.springboot_test.web.dto.PostsSaveRequestDto;
+import example.org.springboot_test.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id){
+        return postsService.findById(id);
+    }
+}
+
